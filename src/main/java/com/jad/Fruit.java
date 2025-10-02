c9e747d0a390ae7a5beb9f48dacd2cac639d430e
adril <adril@MSI>
2025-10-02 09:51:06 +0200

    Fruit naaammee nem

---
+++ src/main/java/com/jad/Fruit.java
@@ -0,0 +1,14 @@
+package com.jad;
+
+public class Fruit {
+
+    private final String name;
+
+    public Fruit(final String name) {
+        this.name = name;
+    }
+
+    public String getName() {
+        return name;
+    }
+}
