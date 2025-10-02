dc16ee1b66f55db17c96150a1732eca83c24750b
Laiko <adrien.gillet@student.junia.com>
2025-10-02 09:58:44 +0200

    Dev fruit (#1)
    * SSOOOOUUUUUUTTTT
    
    * Fruit naaammee nem
    
    * Fruit naaammee nem
    
    * ARRA
    
    ---------
    
    Co-authored-by: adril <adril@MSI>
    Co-authored-by: Celian-Arbona <celian.arbona@student.junia.com>

---
+++ src/main/java/com/jad/Fruit.java
@@ -0,0 +1,15 @@
+package com.jad;
+
+public class Fruit {
+
+    private final String name;
+
+    public Fruit(final String name) {
+        this.name = name;
+    }
+
+    public String getName() {
+        return name;
+        //ARARARARARARARARARARARARARA
+    }
+}
